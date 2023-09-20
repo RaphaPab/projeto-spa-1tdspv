fdb3ed888fa7fd1ae8f41ee66c77ba99b087d48f
rm98525 <rm98525@fiap.com.br>
2023-09-19 22:17:12 -0300

    sdfsf

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 
-//BLOCO DE ROTAS
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './routes/Home.jsx';
 import Produtos from './routes/Produtos.jsx';
-import Erro from './routes/Erro.jsx';
-// import EditarProdutos from './routes/EditarProdutos.jsx';
-import Modal from './components/Modal.jsx';
-import ModalExcluir from './components/ModalExcluir.jsx';
+import EditarProdutos from './routes/EditarProdutos.jsx';
+import Error from './routes/Error.jsx';
+import ExcluirProdutos from './routes/ExcluirProdutos.jsx';
+
+
 
 const router = createBrowserRouter(
   [
@@ -18,8 +18,8 @@ const router = createBrowserRouter(
     children:[
       {path : "/" , element : <Home/>},
       {path : "/produtos" , element : <Produtos/>},
-      {path : "/editar/produtos/:id" , element : <Modal/>},
-      {path : "/excluir/produtos/:id" , element : <ModalExcluir/>},
+      {path : "/editar/produtos/:id" , element : <EditarProdutos/>},
+      {path : "/excluir/produtos/:id" , element : <ExcluirProdutos/>},
     ]}
   ]
 );
