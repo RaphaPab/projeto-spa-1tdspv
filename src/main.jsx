933b00505475d6a506a6d06f1682c1eaed4cb811
rm98525 <rm98525@fiap.com>
2023-09-21 22:57:22 -0300

    inserirproduto

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
@@ -8,26 +8,22 @@ import Produtos from './routes/Produtos.jsx';
 import EditarProdutos from './routes/EditarProdutos.jsx';
 import Error from './routes/Error.jsx';
 import ExcluirProdutos from './routes/ExcluirProdutos.jsx';
+import InserirProduto from './routes/InserirProduto.jsx';
 
-
-
-const router = createBrowserRouter(
-  [
-    {path : "/", element : <App/>, errorElement : <Erro/>,
-    
+const router = createBrowserRouter([
+  {path: "/" , element: <App/>, errorElement: <Error/>,
     children:[
-      {path : "/" , element : <Home/>},
-      {path : "/produtos" , element : <Produtos/>},
-      {path : "/editar/produtos/:id" , element : <EditarProdutos/>},
-      {path : "/excluir/produtos/:id" , element : <ExcluirProdutos/>},
+      {path: "/" , element: <Home/>},
+      {path: "/produtos" , element: <Produtos/>},
+      {path: "/editar/produtos/:id" , element: <EditarProdutos/>},
+      {path: "/excluir/produtos/:id" , element: <ExcluirProdutos/>},
+      {path: "/cadastrar/produto" , element: <InserirProduto/>},
     ]}
-  ]
-);
-
-//BLOCO DE ROTAS
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+    
     <RouterProvider router={router}/>
-  </React.StrictMode>,
+  ,
 )
+
