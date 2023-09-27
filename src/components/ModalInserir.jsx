8ec4b3725a15605f799ebbff5e39f4d286669125
rm98525 <rm98525@fiap.com.br>
2023-09-26 23:02:41 -0300

    MODAL CSS

--- src/components/ModalInserir.jsx
+++ src/components/ModalInserir.jsx
@@ -1,10 +1,60 @@
+import styles from "./ModalInserir.module.css";
+
 export default function ModalInserir(props) {
-    if (props.open) {
-      return (
+  if (props.open) {
+    return (
         <div>
-          <h1>Modal Inserir</h1>
+          <div>
+            <form onSubmit={handleSubmit}>
+              <fieldset>
+                <legend>Cadastrar Produto</legend>
+                <div>
+                  <label htmlFor="idProd">Nome do Produto</label>
+                  <input
+                    type="text"
+                    name="nome"
+                    id="idProd"
+                    onChange={handleChange}
+                    value={produto.nome}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="idDesc">Descrição</label>
+                  <input
+                    type="text"
+                    name="desc"
+                    id="idDesc"
+                    onChange={handleChange}
+                    value={produto.desc}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="idPreco">Preço</label>
+                  <input
+                    type="text"
+                    name="preco"
+                    id="idPreco"
+                    onChange={handleChange}
+                    value={produto.preco}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="idImg">Imagem</label>
+                  <input
+                    type="url"
+                    name="img"
+                    id="idImg"
+                    onChange={handleChange}
+                    value={produto.img}
+                  />
+                </div>
+                <div>
+                  <button>CADASTRAR</button>
+                </div>
+              </fieldset>
+            </form>
+          </div>
         </div>
       );
-    }
   }
-  
+}
